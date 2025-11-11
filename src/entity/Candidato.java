bd06d1408151c3147da46916f04a20b3a2e39c26
guisgiacomini <157508090+guisgiacomini@users.noreply.github.com>
2025-11-11 00:36:42 +0000

    create:
    AfiliacaoController
    Candidato
    PedidoAfiliacao
    Perfil

---
+++ src/entity/Candidato.java
@@ -0,0 +1,12 @@
+package entity;
+
+public class Candidato {
+    String nome;
+    Character sexo;
+    Date dataNascimento;
+    String nacionalidade;
+    String endereco;
+    String profissao;
+    String situacao;
+    Perfil perfil;
+}
